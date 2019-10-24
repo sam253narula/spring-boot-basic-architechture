6e156df2698a698a65fdd83e467ecee611842d74
sam253narula <samarthnarula253@gmail.com>
2019-10-24 23:21:02 +0530

    second commit

--- src/main/java/com/spring/boot/design/pattern/factory/dto/OperationDto.java
+++ src/main/java/com/spring/boot/design/pattern/factory/dto/OperationDto.java
@@ -1,14 +1,5 @@
 package com.spring.boot.design.pattern.factory.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Builder
 public class OperationDto {
 
 	String type;
