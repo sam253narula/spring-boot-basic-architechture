979b165c9078bdc4ab2e931b430851fcecbbf699
sam253narula <samarthnarula253@gmail.com>
2019-11-09 13:51:57 +0530

    "Lombok code"

--- src/main/java/com/spring/boot/learning/lombok/controller/ExampleController.java
+++ src/main/java/com/spring/boot/learning/lombok/controller/ExampleController.java
@@ -2,11 +2,16 @@ package com.spring.boot.learning.lombok.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.boot.learning.lombok.dto.MyIntroductionDTO;
 import com.spring.boot.learning.lombok.service.ExampleService;
 
 @RestController
+@RequestMapping("/LombokDemo")
 public class ExampleController {
 
 	@Autowired
@@ -15,5 +20,11 @@ public class ExampleController {
 	@GetMapping("/hello")
 	public String Hello() {
 		return example.Hello();
+	}
+
+	@PostMapping("/myInfo/{name}/{experiance}/{skills}")
+	public MyIntroductionDTO myInfo(@PathVariable String name, @PathVariable String experiance, @PathVariable String skills) {
+		return MyIntroductionDTO.builder().name(name).experiance(experiance).skills(skills).build();
+		
 	}
 }
